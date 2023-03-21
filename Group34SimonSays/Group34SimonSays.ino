99080fe74f491e4ace92507b8090bdee42a7202d
maiuryn <sebastianho0311@gmail.com>
2023-03-21 12:32:09 -0500

    Completed Graphic Elements

--- Group34SimonSays/Group34SimonSays.ino
+++ Group34SimonSays/Group34SimonSays.ino
@@ -35,26 +35,230 @@ MCUFRIEND_kbv tft;
 int screenWidth;
 int screenHeight;
 
+// Input
 int incomingByte = 0;
 
 // Player 1
 int playerOneState = 0;
 uint16_t playerOneColor = WHITE;
+int playerOneX;
+int playerOneY;
+int playerOneLives;
 
 // Player 2
 int playerTwoState = 0;
 uint16_t playerTwoColor = WHITE;
+int playerTwoX;
+int playerTwoY;
+int playerTwoLives;
+
+// CPU
+int cpuState = 0;
+uint16_t cpuColor = WHITE;
+int cpuX;
+int cpuY;
+
+// Sizes
+int arrowSize = 15;
+int cpuArrowSize = 25;
+int lifeSize = 10;
+
+void drawPlayerOne()
+{
+  tft.fillCircle(playerOneX, playerOneY, 40, playerOneColor);
+
+  if(playerOneState == 1)
+  {
+    tft.fillTriangle(playerOneX - arrowSize, playerOneY + arrowSize, playerOneX + arrowSize, playerOneY + arrowSize, playerOneX, playerOneY - arrowSize, BLACK);     
+  }
+  if(playerOneState == 2)
+  {
+    tft.fillTriangle(playerOneX + arrowSize, playerOneY - arrowSize, playerOneX + arrowSize, playerOneY + arrowSize, playerOneX - arrowSize, playerOneY, BLACK);     
+  }
+  if(playerOneState == 3)
+  {
+    tft.fillTriangle(playerOneX - arrowSize, playerOneY - arrowSize, playerOneX + arrowSize, playerOneY - arrowSize, playerOneX, playerOneY + arrowSize, BLACK);     
+  }  
+  if(playerOneState == 4)
+  {
+    tft.fillTriangle(playerOneX - arrowSize, playerOneY - arrowSize, playerOneX - arrowSize, playerOneY + arrowSize, playerOneX + arrowSize, playerOneY, BLACK);     
+  }  
+}
+
+void drawPlayerTwo()
+{
+  tft.fillCircle(playerTwoX, playerTwoY, 40, playerTwoColor);
+
+  if(playerTwoState == 1)
+  {
+    tft.fillTriangle(playerTwoX - arrowSize, playerTwoY + arrowSize, playerTwoX + arrowSize, playerTwoY + arrowSize, playerTwoX, playerTwoY - arrowSize, BLACK);     
+  }
+  if(playerTwoState == 2)
+  {
+    tft.fillTriangle(playerTwoX + arrowSize, playerTwoY - arrowSize, playerTwoX + arrowSize, playerTwoY + arrowSize, playerTwoX - arrowSize, playerTwoY, BLACK);     
+  }
+  if(playerTwoState == 3)
+  {
+    tft.fillTriangle(playerTwoX - arrowSize, playerTwoY - arrowSize, playerTwoX + arrowSize, playerTwoY - arrowSize, playerTwoX, playerTwoY + arrowSize, BLACK);     
+  }  
+  if(playerTwoState == 4)
+  {
+    tft.fillTriangle(playerTwoX - arrowSize, playerTwoY - arrowSize, playerTwoX - arrowSize, playerTwoY + arrowSize, playerTwoX + arrowSize, playerTwoY, BLACK);     
+  }
+
+}
+
+void drawCPU()
+{
+  tft.fillCircle(cpuX, cpuY, 50, cpuColor);
+
+  if(cpuState == 1)
+  {
+    tft.fillTriangle(cpuX - cpuArrowSize, cpuY + cpuArrowSize, cpuX + cpuArrowSize, cpuY + cpuArrowSize, cpuX, cpuY - cpuArrowSize, ~cpuColor);     
+  }
+  if(cpuState == 2)
+  {
+    tft.fillTriangle(cpuX + cpuArrowSize, cpuY - cpuArrowSize, cpuX + cpuArrowSize, cpuY + cpuArrowSize, cpuX - cpuArrowSize, cpuY, ~cpuColor);     
+  }
+  if(cpuState == 3)
+  {
+    tft.fillTriangle(cpuX - cpuArrowSize, cpuY - cpuArrowSize, cpuX + cpuArrowSize, cpuY - cpuArrowSize, cpuX, cpuY + cpuArrowSize, ~cpuColor);     
+  }  
+  if(cpuState == 4)
+  {
+    tft.fillTriangle(cpuX - cpuArrowSize, cpuY - cpuArrowSize, cpuX - cpuArrowSize, cpuY + cpuArrowSize, cpuX + cpuArrowSize, cpuY, ~cpuColor);     
+  }
+}
+
+void drawLives()
+{
+  for(int i = 0; i < playerOneLives; i++)
+  {
+    tft.fillCircle(lifeSize + (i * lifeSize * 3), screenHeight - lifeSize - 1, lifeSize, RED);
+  }
+
+  for(int i = 0; i < playerTwoLives; i++)
+  {
+    tft.fillCircle(screenWidth - (lifeSize + (i * lifeSize * 3)), screenHeight - lifeSize - 1, lifeSize, RED);
+  }
+}
+
+void draw()
+{
+  tft.fillScreen(BLACK);    
+
+  drawPlayerOne();
+  drawPlayerTwo();
+  drawLives();
+  drawCPU();
+}
+
+
+void getPlayerOneInput()
+{
+  if(incomingByte == 'w')
+  {
+    playerOneColor = RED;
+    playerOneState = 1;
+    draw();
+  }
+  if(incomingByte == 'a')
+  {
+    playerOneColor = BLUE;
+    playerOneState = 2;
+    draw();
+  }
+  if(incomingByte == 's')
+  {
+    playerOneColor = GREEN;
+    playerOneState = 3;
+    draw();
+  }
+  if(incomingByte == 'd')
+  {
+    playerOneColor = YELLOW;
+    playerOneState = 4;
+    draw();
+  }
+  if(incomingByte == 'f')
+  {
+    playerOneColor = CYAN;
+    playerOneState = 5;
+    draw();
+  }
+  if(incomingByte == 'g')
+  {
+    playerOneColor = MAGENTA;
+    playerOneState = 6;
+    draw();
+  }
+  
+}
+
+void getPlayerTwoInput()
+{
+  if(incomingByte == 'i')
+  {
+    playerTwoColor = RED;
+    playerTwoState = 1;
+    draw();
+  }
+  if(incomingByte == 'j')
+  {
+    playerTwoColor = BLUE;
+    playerTwoState = 2;
+    draw();
+  }
+  if(incomingByte == 'k')
+  {
+    playerTwoColor = GREEN;
+    playerTwoState = 3;
+    draw();
+  }
+  if(incomingByte == 'l')
+  {
+    playerTwoColor = YELLOW;
+    playerTwoState = 4;
+    draw();
+  } 
+  if(incomingByte == ';')
+  {
+    playerTwoColor = CYAN;
+    playerTwoState = 5;
+    draw();
+  }
+  if(incomingByte == '\'')
+  {
+    playerTwoColor = MAGENTA;
+    playerTwoState = 6;
+    draw();
+  }
+
+}
 
 void setup() {
   Serial.begin(9600);  // start serial for output
   // uint16_t ID = tft.readID();
-    
+
+  // Screen 
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(BLACK);
-
   screenWidth = tft.width();
   screenHeight = tft.height();
+
+  // Game positions
+  playerOneX = screenWidth / 4;
+  playerOneY = screenHeight * 2 / 3;
+  playerTwoX = screenWidth * 3 / 4;
+  playerTwoY = screenHeight * 2 / 3;
+  cpuX = screenWidth / 2;
+  cpuY = 60;
+
+  // Game values
+  playerOneLives = 3;
+  playerTwoLives = 3;
+  draw();
 }
 
 void loop() {
@@ -65,61 +269,11 @@ void loop() {
     incomingByte = Serial.read();
   }
 
-  // Player One
-  if(incomingByte == 'w')
-  {
-    playerOneColor = RED;
-  }
-  if(incomingByte == 'a')
-  {
-    playerOneColor = BLUE;
-  }
-  if(incomingByte == 's')
-  {
-    playerOneColor = GREEN;
-  }
-  if(incomingByte == 'd')
-  {
-    playerOneColor = YELLOW;
-  }
-  if(incomingByte == 'f')
-  {
-    playerOneColor = CYAN;
-  }
-  if(incomingByte == 'g')
-  {
-    playerOneColor = MAGENTA;
-  }
+  getPlayerOneInput();
+  getPlayerTwoInput();
 
-  // Player Two
-  if(incomingByte == 'i')
-  {
-    playerTwoColor = RED;
-  }
-  if(incomingByte == 'j')
-  {
-    playerTwoColor = BLUE;
-  }
-  if(incomingByte == 'k')
-  {
-    playerTwoColor = GREEN;
-  }
-  if(incomingByte == 'l')
-  {
-    playerTwoColor = YELLOW;
-  } 
-  if(incomingByte == ';')
-  {
-    playerTwoColor = CYAN;
-  }
-  if(incomingByte == '\'')
-  {
-    playerTwoColor = MAGENTA;
-  }
-
+  incomingByte = 0; // Clear Input
+  
   // tft.drawChar(200, 50, incomingByte, YELLOW, BLACK, 2);
-  tft.drawCircle(screenWidth / 2, 50, 50, MAGENTA);
-  tft.drawCircle(screenWidth / 4, screenHeight * 2 / 3, 50, playerOneColor);
-  tft.drawCircle(screenWidth * 3 / 4, screenHeight * 2 / 3, 50, playerTwoColor);
   
 }
