398cedc5ff94c8a915ac9114bb00d581a1e87d23
IsaacS2Hud <79726528+IsaacS2@users.noreply.github.com>
2023-03-21 18:01:11 -0500

    Updated SimonSays Game (Protoytype Complete)

--- Group34SimonSays/Group34SimonSays.ino
+++ Group34SimonSays/Group34SimonSays.ino
@@ -28,6 +28,30 @@ MCUFRIEND_kbv tft;
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
+#define FAKECOLOR 0xC638
+
+//
+// Player State Values
+//
+#define IDLE 1
+#define RIGHT 2
+#define LEFT 3
+#define UP 4
+#define DOWN 5
+#define BUTTON 6
+#define FAKE 7
+#define LOST 8
+#define WIN 9
+#define CORRECT 10 // correct input for the specific round
+#define LIVELOST 11
+#define DRAW 12
+#define NONE 13
+#define FAKEUP 14
+#define FAKELEFT 15
+#define FAKEDOWN 16
+#define FAKERIGHT 17
+#define FAKEBUTTON 18
+
 
 #include <Wire.h>
 
@@ -39,21 +63,25 @@ int screenHeight;
 int incomingByte = 0;
 
 // Player 1
-int playerOneState = 0;
+int playerOneState = IDLE;
+int playerOneResult = NONE;
 uint16_t playerOneColor = WHITE;
 int playerOneX;
 int playerOneY;
 int playerOneLives;
+int playerOneFake = 1;
 
 // Player 2
-int playerTwoState = 0;
+int playerTwoState = IDLE;
+int playerTwoResult = NONE;
 uint16_t playerTwoColor = WHITE;
 int playerTwoX;
 int playerTwoY;
 int playerTwoLives;
+int playerTwoFake = 1;
 
 // CPU
-int cpuState = 0;
+int cpuState = IDLE;
 uint16_t cpuColor = WHITE;
 int cpuX;
 int cpuY;
@@ -63,70 +91,96 @@ int arrowSize = 15;
 int cpuArrowSize = 25;
 int lifeSize = 10;
 
+// Timer
+unsigned long fakeStart = 0; // will store last time the timer started
+unsigned long timer = 3000;
+unsigned long maxCPUDelay = 3000;  // maximum time before CPU
+unsigned long cpuDelay;
+unsigned long roundTime;
+unsigned long downTime = 2000;
+unsigned long fakeDelay = 2000;
+unsigned long roundStart;
+unsigned long resultScreenDelay = 5000;
+
+// cpuDelay = random(maxCPUDelay)
+// Round time = cpuDelay + timer
+// roundTime -> downTime -> ...
+
 void drawPlayerOne()
 {
   tft.fillCircle(playerOneX, playerOneY, 40, playerOneColor);
 
-  if(playerOneState == 1)
+  if(playerOneState == UP)
   {
     tft.fillTriangle(playerOneX - arrowSize, playerOneY + arrowSize, playerOneX + arrowSize, playerOneY + arrowSize, playerOneX, playerOneY - arrowSize, BLACK);     
   }
-  if(playerOneState == 2)
+  if(playerOneState == LEFT)
   {
     tft.fillTriangle(playerOneX + arrowSize, playerOneY - arrowSize, playerOneX + arrowSize, playerOneY + arrowSize, playerOneX - arrowSize, playerOneY, BLACK);     
   }
-  if(playerOneState == 3)
+  if(playerOneState == DOWN)
   {
     tft.fillTriangle(playerOneX - arrowSize, playerOneY - arrowSize, playerOneX + arrowSize, playerOneY - arrowSize, playerOneX, playerOneY + arrowSize, BLACK);     
   }  
-  if(playerOneState == 4)
+  if(playerOneState == RIGHT)
   {
     tft.fillTriangle(playerOneX - arrowSize, playerOneY - arrowSize, playerOneX - arrowSize, playerOneY + arrowSize, playerOneX + arrowSize, playerOneY, BLACK);     
-  }  
+  } 
+  if(playerOneState == BUTTON)
+  {
+    tft.fillCircle(playerOneX, playerOneY, 30, BLACK);
+  } 
 }
 
 void drawPlayerTwo()
 {
   tft.fillCircle(playerTwoX, playerTwoY, 40, playerTwoColor);
 
-  if(playerTwoState == 1)
+  if(playerTwoState == UP)
   {
     tft.fillTriangle(playerTwoX - arrowSize, playerTwoY + arrowSize, playerTwoX + arrowSize, playerTwoY + arrowSize, playerTwoX, playerTwoY - arrowSize, BLACK);     
   }
-  if(playerTwoState == 2)
+  if(playerTwoState == LEFT)
   {
     tft.fillTriangle(playerTwoX + arrowSize, playerTwoY - arrowSize, playerTwoX + arrowSize, playerTwoY + arrowSize, playerTwoX - arrowSize, playerTwoY, BLACK);     
   }
-  if(playerTwoState == 3)
+  if(playerTwoState == DOWN)
   {
     tft.fillTriangle(playerTwoX - arrowSize, playerTwoY - arrowSize, playerTwoX + arrowSize, playerTwoY - arrowSize, playerTwoX, playerTwoY + arrowSize, BLACK);     
   }  
-  if(playerTwoState == 4)
+  if(playerTwoState == RIGHT)
   {
     tft.fillTriangle(playerTwoX - arrowSize, playerTwoY - arrowSize, playerTwoX - arrowSize, playerTwoY + arrowSize, playerTwoX + arrowSize, playerTwoY, BLACK);     
   }
-
+  if(playerTwoState == BUTTON)
+  {
+    tft.fillCircle(playerTwoX, playerTwoY, 30, BLACK);
+  }
 }
 
 void drawCPU()
 {
   tft.fillCircle(cpuX, cpuY, 50, cpuColor);
 
-  if(cpuState == 1)
+  if(cpuState == UP || cpuState == FAKEUP)
   {
     tft.fillTriangle(cpuX - cpuArrowSize, cpuY + cpuArrowSize, cpuX + cpuArrowSize, cpuY + cpuArrowSize, cpuX, cpuY - cpuArrowSize, ~cpuColor);     
   }
-  if(cpuState == 2)
+  if(cpuState == LEFT || cpuState == FAKELEFT)
   {
     tft.fillTriangle(cpuX + cpuArrowSize, cpuY - cpuArrowSize, cpuX + cpuArrowSize, cpuY + cpuArrowSize, cpuX - cpuArrowSize, cpuY, ~cpuColor);     
   }
-  if(cpuState == 3)
+  if(cpuState == DOWN || cpuState == FAKEDOWN)
   {
     tft.fillTriangle(cpuX - cpuArrowSize, cpuY - cpuArrowSize, cpuX + cpuArrowSize, cpuY - cpuArrowSize, cpuX, cpuY + cpuArrowSize, ~cpuColor);     
   }  
-  if(cpuState == 4)
+  if(cpuState == RIGHT || cpuState == FAKERIGHT)
   {
     tft.fillTriangle(cpuX - cpuArrowSize, cpuY - cpuArrowSize, cpuX - cpuArrowSize, cpuY + cpuArrowSize, cpuX + cpuArrowSize, cpuY, ~cpuColor);     
+  }
+  if(cpuState == BUTTON || cpuState == FAKEBUTTON)
+  {
+    tft.fillCircle(cpuX, cpuY, 40, BLACK);
   }
 }
 
@@ -143,6 +197,27 @@ void drawLives()
   }
 }
 
+void winScreen()
+{
+  tft.fillScreen(0x07F2);
+  tft.setCursor(50, screenHeight / 2 - 5);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(3);
+  if(playerTwoLives == 0)  
+    tft.print("Player 1 Wins!");
+  if(playerOneLives == 0)
+    tft.print("Player 2 Wins!");
+}
+
+void tieScreen()
+{
+  tft.fillScreen(0xFEE0);
+  tft.setCursor(75, screenHeight / 2 - 5);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(3);
+  tft.print("Tie Game!");
+}
+
 void draw()
 {
   tft.fillScreen(BLACK);    
@@ -156,92 +231,242 @@ void draw()
 
 void getPlayerOneInput()
 {
-  if(incomingByte == 'w')
-  {
-    playerOneColor = RED;
-    playerOneState = 1;
-    draw();
+  if (playerOneState == IDLE && (millis() - roundStart < roundTime) ) {
+    if(incomingByte == 'w')
+    {
+      playerOneColor = RED;
+      playerOneState = UP;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'a')
+    {
+      playerOneColor = BLUE;
+      playerOneState = LEFT;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 's')
+    {
+      playerOneColor = GREEN;
+      playerOneState = DOWN;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'd')
+    {
+      playerOneColor = YELLOW;
+      playerOneState = RIGHT;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'f')
+    {
+      playerOneColor = CYAN;
+      playerOneState = BUTTON;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'g')
+    {
+      playerOneColor = WHITE;
+      playerOneState = FAKE;
+      gameLogic();
+      //draw();
+    }
   }
-  if(incomingByte == 'a')
-  {
-    playerOneColor = BLUE;
-    playerOneState = 2;
-    draw();
-  }
-  if(incomingByte == 's')
-  {
-    playerOneColor = GREEN;
-    playerOneState = 3;
-    draw();
-  }
-  if(incomingByte == 'd')
-  {
-    playerOneColor = YELLOW;
-    playerOneState = 4;
-    draw();
-  }
-  if(incomingByte == 'f')
-  {
-    playerOneColor = CYAN;
-    playerOneState = 5;
-    draw();
-  }
-  if(incomingByte == 'g')
-  {
-    playerOneColor = MAGENTA;
-    playerOneState = 6;
-    draw();
-  }
-  
 }
 
 void getPlayerTwoInput()
 {
-  if(incomingByte == 'i')
-  {
-    playerTwoColor = RED;
-    playerTwoState = 1;
-    draw();
+  if (playerTwoState == IDLE && (millis() - roundStart < roundTime) ) {
+    if(incomingByte == 'i')
+    {
+      playerTwoColor = RED;
+      playerTwoState = UP;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'j')
+    {
+      playerTwoColor = BLUE;
+      playerTwoState = LEFT;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'k')
+    {
+      playerTwoColor = GREEN;
+      playerTwoState = DOWN;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == 'l')
+    {
+      playerTwoColor = YELLOW;
+      playerTwoState = RIGHT;
+      gameLogic();
+      draw();
+    } 
+    if(incomingByte == ';')
+    {
+      playerTwoColor = CYAN;
+      playerTwoState = BUTTON;
+      gameLogic();
+      draw();
+    }
+    if(incomingByte == '\'')
+    {
+      playerTwoColor = WHITE;
+      playerTwoState = FAKE;
+      gameLogic();
+      //draw();
+    }
   }
-  if(incomingByte == 'j')
-  {
-    playerTwoColor = BLUE;
-    playerTwoState = 2;
-    draw();
-  }
-  if(incomingByte == 'k')
-  {
-    playerTwoColor = GREEN;
-    playerTwoState = 3;
-    draw();
-  }
-  if(incomingByte == 'l')
-  {
-    playerTwoColor = YELLOW;
-    playerTwoState = 4;
-    draw();
-  } 
-  if(incomingByte == ';')
-  {
-    playerTwoColor = CYAN;
-    playerTwoState = 5;
-    draw();
-  }
-  if(incomingByte == '\'')
-  {
-    playerTwoColor = MAGENTA;
-    playerTwoState = 6;
-    draw();
-  }
+}
 
+//
+// timeOut
+//
+// This function will check the states of the players if the current game's
+// timer runs out.
+//
+void timeOut() {
+    if (playerOneResult != CORRECT && playerOneResult != LIVELOST && playerOneResult != LOST) {
+      if (playerOneLives <= 1) {  // Player 1 lost
+        playerOneResult = LOST;
+      } 
+      else {  // Player 1 loses a life
+        playerOneResult = LIVELOST;
+      }
+      playerOneLives--;
+    }
+
+    if (playerTwoResult != CORRECT && playerTwoResult != LIVELOST && playerTwoResult != LOST) {
+      if (playerTwoLives <= 1) {  // Player 2 lost
+        playerTwoResult = LOST;
+      } 
+      else {  // Player 2 loses a life
+        playerTwoResult = LIVELOST;
+      }
+      playerTwoLives--;
+    }
+
+    if (playerOneResult == LOST && playerTwoResult == LOST) {
+        playerOneResult = DRAW;
+        playerTwoResult = DRAW;
+    }
+}
+
+//
+// gameLogic
+//
+// This function will hold the game logic for the game; it uses the state of the players, as well as the state 
+// of the Simon Says CPU, to determine if a player has lost, if a player lost a live, or if the game is continuing.
+//
+void gameLogic() {
+    //
+    // Player 1 has made a move
+    //
+    if (playerOneState != IDLE && playerOneState != FAKE && playerOneResult != LOST && playerOneResult != LIVELOST && playerOneResult != CORRECT) {
+      if (playerOneState != cpuState) {  // Player 1 made a wrong move
+        if (playerOneLives <= 1) {  // Player 1 lost
+          playerOneResult = LOST;
+        } 
+        else {  // Player 1 loses a life
+          playerOneResult = LIVELOST;
+        }
+        playerOneLives--;
+      }
+      else  // Player 1 made the correct move
+      {
+        playerOneResult = CORRECT;
+      }
+    }
+    else if (playerOneState == FAKE && cpuState == IDLE && playerOneFake)
+    {
+        roundTime += fakeDelay;
+        cpuDelay += fakeDelay;
+        playerOneFake = 0;
+        playerOneState = IDLE;
+        fakeStart = millis();
+        
+        cpuColor = FAKECOLOR;
+        cpuState = random(FAKERIGHT, FAKEBUTTON + 1);
+        draw();
+    }
+
+    //
+    // Player 2 has made a move
+    //
+    if (playerTwoState != IDLE && playerTwoState != FAKE && playerTwoResult != LOST && playerTwoResult != LIVELOST && playerTwoResult != CORRECT) {
+      if (playerTwoState != cpuState) {  // Player 2 made a wrong move
+        if (playerTwoLives <= 1) {  // Player 2 lost
+          playerTwoResult = LOST;
+        } 
+        else {  // Player 2 loses a life
+          playerTwoResult = LIVELOST;
+        }
+        playerTwoLives--;
+      }
+      else  // Player 1 made the correct move
+      {
+        playerTwoResult = CORRECT;
+      }
+    }
+    else if (playerTwoState == FAKE && cpuState == IDLE && playerTwoFake)
+    {
+        roundTime += fakeDelay;
+        cpuDelay += fakeDelay;
+        playerTwoFake = 0;
+        playerTwoState = IDLE;
+        fakeStart = millis();
+        
+        cpuColor = FAKECOLOR;
+        cpuState = random(FAKERIGHT, FAKEBUTTON + 1);
+        draw(); 
+    }
+    
+}
+
+//
+// newRound
+//
+// This function will initialize a new round of the game
+//
+void newRound() {
+  cpuDelay = random(0, maxCPUDelay);
+  playerOneState = IDLE;
+  playerOneResult = NONE;
+  playerOneColor = WHITE;
+  playerTwoState = IDLE;
+  playerTwoResult = NONE;
+  playerTwoColor = WHITE;
+  cpuState = IDLE;
+  cpuColor = WHITE;
+  draw();
+  roundStart = millis();
+  roundTime = cpuDelay + timer;
+}
+
+//
+// resetGame
+//
+// This function resets the entire game, including resetting players values
+//
+void resetGame() {
+  playerOneLives = 3;
+  playerTwoLives = 3;
+  playerOneFake = 1;
+  playerTwoFake = 1;
 }
 
 void setup() {
   Serial.begin(9600);  // start serial for output
-  // uint16_t ID = tft.readID();
+  uint16_t ID = tft.readID();
 
   // Screen 
-  tft.begin();
+  tft.begin(ID);
   tft.setRotation(1);
   tft.fillScreen(BLACK);
   screenWidth = tft.width();
@@ -255,10 +480,9 @@ void setup() {
   cpuX = screenWidth / 2;
   cpuY = 60;
 
-  // Game values
-  playerOneLives = 3;
-  playerTwoLives = 3;
+  resetGame();
   draw();
+  newRound();
 }
 
 void loop() {
@@ -269,11 +493,53 @@ void loop() {
     incomingByte = Serial.read();
   }
 
+  if(millis() - roundStart > roundTime)
+  { 
+    timeOut();
+    delay(downTime);
+
+    if(playerOneLives == 0 && playerTwoLives == 0)
+    {
+      for(int i = 0; i < 10; i++)
+        tieScreen();
+      delay(resultScreenDelay);
+      resetGame();
+      
+    }
+    else if(playerOneLives == 0 || playerTwoLives == 0)
+    {
+      for(int i = 0; i < 10; i++)
+        winScreen();
+      delay(resultScreenDelay);
+      resetGame();
+      
+    }
+    
+    newRound();
+    while(Serial.available() > 0)
+      Serial.read();      
+    incomingByte = 0;
+
+  }
+
+  // the CPU's state is now changed
+  if((millis() - roundStart > roundTime - timer) && cpuState == IDLE )
+  {
+    cpuState = random(RIGHT, BUTTON + 1);
+    draw();
+  }
+
+  // the CPU is no longer fake
+  if((millis() - fakeStart > fakeDelay) && (cpuState >= FAKEUP && cpuState <= FAKEBUTTON))
+  {
+    cpuState = IDLE;
+    cpuColor = WHITE;
+    draw();
+  }
+  
   getPlayerOneInput();
   getPlayerTwoInput();
 
   incomingByte = 0; // Clear Input
-  
-  // tft.drawChar(200, 50, incomingByte, YELLOW, BLACK, 2);
   
 }
